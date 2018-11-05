a97d236541535d9224231c626d0bbea9db82efa5
unknown <garengit1@gmail.com>
2018-11-05 14:19:46 +0700

    create function koneksi to database

---
+++ Notification/src/koneksi.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class koneksi {
+private static Connection mysqlcon;
+public static Connection koneksiDB() throws  SQLException{
+   if(mysqlcon==null){
+       try {
+           String DB = "jdbc:mysql://localhost:3306/delta_db";
+           String user="root";
+           String pass="";
+           DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+           mysqlcon = (Connection) DriverManager.getConnection(DB,user,pass);
+       } catch(Exception e) {
+           JOptionPane.showMessageDialog(null, "Gagal Koneksi");
+       }
+   }
+   return mysqlcon; 
+}
+}
